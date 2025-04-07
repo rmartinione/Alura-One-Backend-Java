8fb3c6a6bcd401c79317c1bf220b75d9c6aa1916
rmartini3corp <rm_corporate@hotmail.com>
2025-04-06 22:19:14 -0300

    V1 - Primeiro Projeto criação de uma aplicação usando Linguagem Java.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Compliando impressão de boas vindas!
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Olá Rafael!");
+        System.out.println("Tudo bem?");
+
+    public class Estudos {
+        
+    }
+  }
+}
